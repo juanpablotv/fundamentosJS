787d5563dda4a5762a002fe2450e2d58131314ed
juanpablotv <torresvaldesjuanpablo@gmail.com>
2023-09-08 14:25:44 -0600

    Comenzando con JS

---
+++ tipos.js
@@ -0,0 +1,28 @@
+console.log("Hola Desde Node");
+
+// comentarios de una sola linea
+
+// diferenetes tipos de datos
+
+/*
+number
+string
+boolean
+object
+undefined
+*/
+
+console.log(123)
+console.log(typeof(123))
+
+console.log(123);
+console.log(typeof 123);
+
+console.log(123);
+console.log(typeof 123);
+
+console.log(123)
+console.log(typeof 123);
+
+console.log(123);
+console.log(typeof 123);
