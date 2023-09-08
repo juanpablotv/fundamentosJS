590dcc63af035a047b632d97396d89e21d60a382
juanpablotv <torresvaldesjuanpablo@gmail.com>
2023-09-08 14:42:24 -0600

    02 variables JS

---
+++ variables.js
@@ -0,0 +1,54 @@
+// js no es tipado por lo que no se especifica el tipo de dato
+
+// Para declaracion de variables
+// Utilizamos const y let
+// Const para variables de solo lectura
+// let para variables
+
+const pi = 3.14;
+// No podemos asignar nuevos valores a const
+pi ="tres"
+
+// Debemos inicializar siempre const
+// const g;
+// g = 1.89;
+
+let contador;
+console.log(contador);
+
+contador = 10;
+contador += 1;
+contador += 1;
+contador += 1;
+contador += 1;
+
+console.log (contador);
+
+contador = "ahora spy un string";
+console.log(contador);
+
+let verdad = true;
+console.log(typeof(verdad));
+console.log(typeof (contador));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
