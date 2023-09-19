2324fd75b05849ef9c4cbdf4a7d624396f98e6a5
juanpablotv <torresvaldesjuanpablo@gmail.com>
2023-09-19 11:59:46 -0600

    09 ciclos js

---
+++ ciclos.js
@@ -0,0 +1,40 @@
+// ciclos en javascrip 
+
+// for 
+for(let i =  0; i  < 3; i++){
+    console.log('vuelta ' + i);
+}
+
+// while 
+    
+let contador  = 0; 
+while(contador  < 3){
+    console.log('vuelta' + contador);
+}
+
+// do while  
+ cot
+
+
+
+
+
+ // recorrido de un string
+ const name = 'Peter';
+ for(let i = 0; i name.length; i++){
+    console.log(name[i])
+ }
+
+
+
+
+
+
+ // for .... of 
+ for(const item of numbers){
+    console.log(item);
+ }
+
+for (const letter of name){
+    console.log(letter);
+}
