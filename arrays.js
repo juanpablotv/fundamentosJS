86666bdd128982614f00c58d6ff150c8a287be3d
juanpablotv <torresvaldesjuanpablo@gmail.com>
2023-09-19 11:41:01 -0600

    08 Arrays

---
+++ arrays.js
@@ -0,0 +1,37 @@
+// Arrays en JavaScript
+const miArrray = [1, 2, 4 , true, 'Hello', 'A', 2.1, [1, 2]];
+console.log(miArrray);
+console.log(miArrray[0]);
+console.log(miArrray[1]);
+miArrray[0] = "One";
+miArrray[3], false;
+console.log(miArrray);
+
+// pilas  en js 
+// last in first out 
+// ingresar valores al final 
+// extraer valores del final 
+const pila = [];
+// insertar con push
+pila.push(1);
+pila.push(2);
+pila.push(3);
+console.log(pila);
+console.log(pila.pop());
+console.log(pila);
+console.log(pila.pop());
+console.log(pila);
+
+// Colas en js 
+// first in first out 
+const cola = []
+cola.unshift(1); 
+cola.unshift(2); 
+cola.unshift(3); 
+console.log(cola);
+console.log(cola.shift());
+console.log(cola);
+console.log(cola.shift());
+console.log(cola);
+
+
